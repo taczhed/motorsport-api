b9ddf6972aced78324aff45f03ed2bbbb4b00cfb
taczhed <patryklach99@gmail.com>
2025-05-11 14:49:01 +0200

    feat: entities

---
+++ src/Domain/Entities/Track.cs
@@ -0,0 +1,11 @@
+﻿namespace MotorsportApi.Domain.Entities;
+
+public class Track
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public double LengthKm { get; set; }
+
+    public ICollection<Race> Races { get; set; } = new List<Race>();
+}
