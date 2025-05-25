ed85abd7257f2a0e8968c42ac1eb1b6856fbac95
taczhed <patryklach99@gmail.com>
2025-05-25 13:36:32 +0200

    refactor: modify drivers in races

--- src/Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
+++ src/Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+493168d636fb4769b6c8e50a90f15820afc33833")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88dcaff7a239e257f25158beb3e400e9a2a9304a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
